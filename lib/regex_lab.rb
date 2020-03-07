45855362f94db2879d937df5ebb9924810db01db
Jimmy Le <jimmyle414@gmail.com>
2020-03-07 00:32:33 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -39,7 +39,9 @@ end
   #does the \( mean - any parenthesis, in the same way that \s means any white space
   
 def valid_phone_number?(phone)
-  if phone.match(/([0-9] ?){10}/) || phone.match(/(\([0-9]{3}\)([0-9]{3}-[0-9]{4})\b)/) || phone.match(/\b([0-9]{7})\b/)
+  if phone.match(/([0-9] ?){10}/) 
+    || phone.match(/(\([0-9]{3}\)([0-9]{3}-[0-9]{4})\b)/) 
+    || phone.match(/\b([0-9]{7})\b/)
     true
   else
     false
